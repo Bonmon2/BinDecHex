fd375d47e018fe3a9d847cfeae7c16013a9ec73a
Bonmon2 <benjaminwmoore91@gmail.com>
2020-11-18 23:47:56 +0000

    Create Implementation.py

---
+++ Implementation.py
@@ -0,0 +1,18 @@
+import BinHexDec
+
+Num1 = BinHexDec.Nums(11, 10)
+Num2 = BinHexDec.Nums("12", 10)
+
+print(Num2 - Num1)
+print(Num1 + 1)
+print(Num1 - 12)
+
+Num3 = BinHexDec.Nums(11, 2)
+print(Num3.DecVal)
+Num3.ToType(3)
+print(Num3.DecVal)
+
+Num4 = BinHexDec.Nums(-12, 5)
+print(Num4)
+print(1 + Num4)
+print(1 - Num4)
