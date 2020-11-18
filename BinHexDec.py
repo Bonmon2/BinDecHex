e096da8794f5d6ef5dfb7c27643fec6efb92fd8b
Bonmon2 <benjaminwmoore91@gmail.com>
2020-11-18 22:34:14 +0000

    Create BinHexDec.py

---
+++ BinHexDec.py
@@ -0,0 +1,92 @@
+_alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+_numerals = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+def _valtypecheck(_numtype):
+    if isinstance(_numtype, int):
+        if _numtype > 36:
+            raise ValueError("Sorry BinHexDec currently has now way of using bases over base 36")
+        else:
+            return _numtype
+    elif isinstance(_numtype, float):
+        raise TypeError("NumType cannot accept a type float")
+    elif _numtype.lower() in ["bin", "dec", "hex"]:
+        if _numtype == "bin":
+            return 2
+        elif _numtype == "dec":
+            return 10
+        else:
+            return 16
+    else:
+        raise ValueError("Unknown type idetifier")
+
+class Nums():
+    def __init__(self, NumVal, NumType):
+        
+        if isinstance(NumVal, int):
+            NumVal = str(NumVal)
+
+        self.ValType = _valtypecheck(NumType)
+        self.DecVal = self._toDec(NumVal)
+
+
+    def __add__(self, var2):
+        if isinstance(var2, int):
+            _sumval = self.DecVal + var2
+        elif isinstance(var2, str):
+            for x in range(len(var2)):
+                if var2[x] in _alphabet:
+                    raise TypeError("You can't add a letter to a number, if this letter " + var2[x] + " was meant to to represent a value please use the class so that the base can be identified")
+            _sumval = self.DecVal + int(var2)
+        elif isinstance(var2, float):
+            raise ValueError("Nums cannot accept float values as they are often hard to implement into other bases")
+        else:
+            _sumval = self.DecVal + var2.DecVal
+        return _sumval
+
+    def __sub__(self, var2):
+        _subval = self.DecVal - var2.DecVal
+        return _subval
+    
+    def __str__(self):
+        return str(self.DecVal)
+
+
+    def ToDec(self):
+        return self.DecVal
+
+
+    def _toDec(self, _numval):
+        _decnum = 0
+        for i in range(len(str(_numval))):
+            if _numval[i].lower() in _alphabet:
+                for x in range(26):
+                    if _numval[i].lower() == _alphabet[x]:
+                        _numeraltotal = x + 11
+                        print("pass")
+                        break
+            else:
+                for x in range(10):
+                    if _numval[i] == _numerals[x]:
+                        _numeraltotal = x
+                        break
+            
+            _decnum += _numeraltotal * self.ValType ** (len(str(_numval)) - i - 1)
+        return _decnum
+
+    def ToType(self, base):
+        _valtypecheck(base)
+        returnnum = ""
+        _decval = self.DecVal
+        while _decval > (base - 1):
+            index = 1
+            while (_decval / (base ** index) ) > base:
+                index += 1
+
+            _roundedresult = round(_decval / (base ** index))
+            if isinstance(_roundedresult, int):
+                returnnum = returnnum + str(_roundedresult)
+            else:
+                returnnum = returnnum + _alphabet[_roundedresult - 10]
+            _decval = _decval % (base ** index)
+        returnnum = returnnum + str(_decval)
+        return returnnum
